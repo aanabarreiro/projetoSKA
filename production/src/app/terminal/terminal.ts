cc18188d9d4ad25e7fbad9aa7f0a7ba3e1b51460
Ana Beatriz Arteiro Barreiro <anabeatrizarteiro@gmail.com>
2025-10-16 16:44:47 -0300

    Continuação do projeto de aula

--- production/src/app/terminal/terminal.ts
+++ production/src/app/terminal/terminal.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {ProductionControl} from '../production-control/production-control';
+import {MatDialog, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import {DialogSelect} from '../dialog-select/dialog-select';
+import productionOrders from '../../assets/files/production-orders.json';
 
-@Component({
-  selector: 'app-terminal',
-  imports: [],
-  templateUrl: './terminal.html',
-  styleUrl: './terminal.scss'
+@Component({  //indica que esse componente é um componente angular
+  selector: 'app-terminal', //nome do componente
+  imports: [ProductionControl], //outros componentes que esse componente usa
+  templateUrl: './terminal.html',//caminho do arquivo html do componente
+  styleUrl: './terminal.scss' //caminho do arquivo css do componente
 })
 export class Terminal {
 
+  readonly dialog: MatDialog = inject(MatDialog);
+
+    productionOrders: any[] = productionOrders;
+
+  setProductionOrder() {
+    console.log('Teste de evento click- Produção Iniciada');
+    this.openSelectDialog();
+  }
+
+  openSelectDialog(): any {
+    const dialogRef: MatDialogRef<DialogSelect, any> = this.dialog.open(DialogSelect, {
+      width: '950px',
+      panelClass: 'custom-dialog',
+      data:{
+        dialogTitle: 'Teste dialog', optionsList: this.productionOrders
+      }
+    });
+
+    return new Promise((resolve) => {
+      dialogRef.afterClosed().subscribe((result) => {
+        resolve(result);
+      });
+    });
+  }
 }
